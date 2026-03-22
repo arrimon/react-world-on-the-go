ec9787d88a1f429f13754a44f45d7b6ddf7ba1fa
arrimon <rsnrimon17@gmail.com>
2026-03-22 10:59:33 +0600

    React Core Concepts

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+
+  return (
+    <div>
+      <h1>React World Wide ...</h1>
+      <Countries />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h3>In the List of countries</h3>
+        </div>
+    );
+};
+
+export default Countries;
